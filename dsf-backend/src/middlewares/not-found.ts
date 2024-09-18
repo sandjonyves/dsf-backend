770a293e21e56b901cedf16bc6f05ac657cb3877
sandjonyves <sandjonyves@gmail.com>
2024-09-18 16:17:48 +0000

    remove all cors origin

--- dsf-backend/src/middlewares/not-found.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express"
-
-const notFound = (req:Request, res:Response) => res.status(404).json({status:'error', message:'Route does not exist'})
-
-export default notFound
